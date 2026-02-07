dafc8d89b614bbce05f55591fa6de624a16fe8ab
tmylla <misite_J@163.com>
2026-02-07 19:16:00 +0800

    Fix routing: add base path to wouter Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -10,28 +10,24 @@ import Papers from "./pages/Papers";
 import About from "./pages/About";
 import Layout from "./components/Layout";
 
-function Router() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/findings" component={Findings} />
-        <Route path="/papers" component={Papers} />
-        <Route path="/about" component={About} />
-        <Route path="/404" component={NotFound} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-  );
-}
-
 function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <Router base="/psybernetic">
+            <Layout>
+              <Switch>
+                <Route path="/" component={Home} />
+                <Route path="/findings" component={Findings} />
+                <Route path="/papers" component={Papers} />
+                <Route path="/about" component={About} />
+                <Route path="/404" component={NotFound} />
+                <Route component={NotFound} />
+              </Switch>
+            </Layout>
+          </Router>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
